ba25e7bbbc13905c0ebe2dfd0fa88f5fa1990ff6
Fafruch <fafruszek@gmail.com>
2017-09-26 01:51:14 +0200

    Move header lines to new <Header /> and add <hr />, small fixes in CSS

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <h1>Color reader</h1>
+    <hr className="Header-line" />
+    <h4>See & convert your <br /> HEX / RGB / HSL color</h4>
+  </div>
+);
+
+export default Header;
